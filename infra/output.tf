f7a835018662409c2ff8227bdf49aea4cfe494a9
Mikita Tsitok <mikita.tsitok@innowise.com>
2025-12-25 23:58:53 +0300

    test gitlab:v02

--- infra/output.tf
+++ infra/output.tf
@@ -1,4 +1,4 @@
-output "privat-Ips" {
+output "privatIps" {
   description = "ip adresses for ansible? "
   value = [for instance in google_compute_instance.vm_for_databases[*]: instance.network_interface[0].network_ip]  
   #google_compute_instance.vm_for_databases[*].network_interface[*].network_ip
@@ -9,6 +9,7 @@ output "key-for-artifact-registry" {
   #value = google_service_account_key.registry-access-key.private_key
   value = "key is stored in file)))"
 }
+
 resource "local_file" "private-key-for-registry" {
   content = google_service_account_key.registry-access-key.private_key
   filename = "./private-key.json" 
